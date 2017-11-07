dc98092eedbdef1f8dd14b3052f3156a5e23f629
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-11-07 13:38:24 +0530

    Add Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
-import{ FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import {
+  FormGroup,
+  FormControl,
+  InputGroup,
+  Glyphicon
+} from 'react-bootstrap';
 import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query:'',
-      artist:''
+      query: '',
+      artist: null,
+      tracks: []
     }
   }
 
@@ -16,28 +25,44 @@ class App extends Component {
     console.log(this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
     var accessToken = 'BQA_cKCAdw-a_f2E15dDGK5UKuelEomTehfov6K7kq6ftyXMV36Zv4jGgvWZuC-AdnKmlAa-JXzNJkmMJEk1DQ';
 
 
     var myOptions = {
       method: 'GET',
-      headers:  {
+      headers: {
         'Authorization': 'Bearer ' + accessToken
-     },
+      },
       mode: 'cors',
       cache: 'default'
     };
 
-    fetch(FETCH_URL, myOptions )
+    fetch(FETCH_URL, myOptions)
       .then(response => response.json())
       .then(json => {
         const artist = json.artists.items[0];
-        this.setState({artist});
+        this.setState({
+          artist
+        });
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, myOptions)
+          .then(response => response.json())
+          .then(json => {
+            console.log('artist\'s top tracks:', json);
+            const {
+              tracks
+            } = json;
+            this.setState({
+              tracks
+            });
+          })
       })
   }
 
   render() {
-    return(
+    return (
       <div className="App">
         <div className="App-title">Music Master</div>
         <FormGroup>
@@ -48,22 +73,30 @@ class App extends Component {
               value={this.state.query}
               onChange={event => {this.setState({query: event.target.value})}}
               onKeyPress={event => {
-                if (event.Key === "Enter") {
-                  this.search();
+                if (event.key === 'Enter') {
+                  this.search()
                 }
               }}
             />
-          <InputGroup.Addon onClick={() => this.search()}>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <Profile
-          artist={this.state.artist}
-        />
-        <div className="Gallery">
-          Gallery
-        </div>
+        {
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
+            </div>
+          : <div></div>
+        }
+
       </div>
     )
   }
